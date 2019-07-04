e42064d9dbb28a605e58332f27f4d267ff95cfcd
Shinri Horiwa <horoiwa195@gmail.com>
2019-07-04 23:36:50 +0900

    style ifx

--- model.py
+++ model.py
@@ -48,7 +48,8 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1)):
     drop5 = Dropout(0.5)(conv5)
 
     up6 = Conv2D(512, 2, activation='relu', padding='same',
-                 kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(drop5))
+                 kernel_initializer='he_normal'
+                 )(UpSampling2D(size=(2, 2))(drop5))
     merge6 = concatenate([drop4, up6], axis=3)
     conv6 = Conv2D(512, 3, activation='relu', padding='same',
                    kernel_initializer='he_normal')(merge6)
@@ -56,7 +57,8 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1)):
                    kernel_initializer='he_normal')(conv6)
 
     up7 = Conv2D(256, 2, activation='relu', padding='same',
-                 kernel_initializer='he_normal')(UpSampling2D(size = (2,2))(conv6))
+                 kernel_initializer='he_normal'
+                 )(UpSampling2D(size=(2, 2))(conv6))
     merge7 = concatenate([conv3, up7], axis=3)
     conv7 = Conv2D(256, 3, activation='relu', padding='same',
                    kernel_initializer='he_normal')(merge7)
@@ -64,7 +66,8 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1)):
                    kernel_initializer='he_normal')(conv7)
 
     up8 = Conv2D(128, 2, activation='relu', padding='same',
-                 kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(conv7))
+                 kernel_initializer='he_normal'
+                 )(UpSampling2D(size=(2, 2))(conv7))
     merge8 = concatenate([conv2, up8], axis=3)
     conv8 = Conv2D(128, 3, activation='relu', padding='same',
                    kernel_initializer='he_normal')(merge8)
@@ -72,7 +75,8 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1)):
                    kernel_initializer='he_normal')(conv8)
 
     up9 = Conv2D(64, 2, activation='relu', padding='same',
-                 kernel_initializer='he_normal')(UpSampling2D(size = (2,2))(conv8))
+                 kernel_initializer='he_normal'
+                 )(UpSampling2D(size=(2, 2))(conv8))
     merge9 = concatenate([conv1, up9], axis=3)
     conv9 = Conv2D(64, 3, activation='relu', padding='same',
                    kernel_initializer='he_normal')(merge9)
